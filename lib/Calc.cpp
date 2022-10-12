f9ba5bb39c98499a0a7dcda6e6b0827f13b58f1c
Fabio Lenherr <fabio.lenherr@gmail.com>
2022-10-12 08:46:56 +0200

    fix: ctr+d escapes

--- lib/Calc.cpp
+++ lib/Calc.cpp
@@ -5,56 +5,58 @@
  * exp := term | exp + term | exp - term
  * term := factor | factor * term | factor / term | factor % term
  * factor := number | ( exp )
-*/
+ */
 
 void Calculator::greeting() {
-  std::cout << "A small calulator, various expressions allowed, operators + - / * %.\n Or enter exit to exit\n";
+  std::cout << "A small calulator, various expressions allowed, operators + - "
+               "/ * %.\n Or enter exit to exit\n";
 }
 
-std::string Calculator::handle_vars(std::vector<std::string>& input) {
-  if(input.front() == "add") {
+std::string Calculator::handle_vars(std::vector<std::string> &input) {
+  if (input.front() == "add") {
     input.erase(input.begin());
-    if(input.size() > 1) {
-      return this->write_var(input.at(0),input.at(1));
+    if (input.size() > 1) {
+      return this->write_var(input.at(0), input.at(1));
     } else {
       return "Please enter a variable and a number to assign.\n";
     }
-  } else if(input.front() == "del") {
+  } else if (input.front() == "del") {
     input.erase(input.begin());
-    if(input.size() > 0) {
+    if (input.size() > 0) {
       return this->delete_vars(input.at(0));
     }
   }
-return "";
+  return "";
 }
 
-void Calculator::interface(bool fancy) {
-  this->interface(fancy, std::cin);
-}
+void Calculator::interface(bool fancy) { this->interface(fancy, std::cin); }
 
-void Calculator::interface(bool fancy, std::istream& is) {
+void Calculator::interface(bool fancy, std::istream &is) {
   std::cout << "Enter an expression:\n";
-  std::string line;
-  std::getline(is, line);
-  if(line != "") {
-    if(line == "exit"){
+  std::string line = "";
+  if (std::getline(is, line) && !is.eof()) {
+  } else {
+    return;
+  }
+  if (line != "") {
+    if (line == "exit") {
       return;
     }
     std::vector<std::string> input = Calculator::splitString(line);
     std::string varreturn = this->handle_vars(input);
-    if(varreturn != "") {
-      std::cout << varreturn ;
+    if (varreturn != "") {
+      std::cout << varreturn;
     } else {
       std::string result = this->calculate(input);
-      if(result.front() == '-' || std::isdigit(result.front())) {
-        if(fancy){
+      if (result.front() == '-' || std::isdigit(result.front())) {
+        if (fancy) {
           std::cout << "Result:\n";
-          printDigit(result,std::cout); 
+          printDigit(result, std::cout);
         } else {
           std::cout << "Result: " << result << "\n";
         }
       } else {
-        if(fancy) {
+        if (fancy) {
           printErr(std::cout);
           std::cout << result << "\n";
         } else {
@@ -66,9 +68,9 @@ void Calculator::interface(bool fancy, std::istream& is) {
   interface(fancy);
 }
 
-std::string Calculator::gui(std::string& str, bool var_edit) {
+std::string Calculator::gui(std::string &str, bool var_edit) {
   std::vector<std::string> input = Calculator::splitString(str);
-  if(var_edit) {
+  if (var_edit) {
     return this->handle_vars(input);
   } else {
     return this->calculate(input);
@@ -76,14 +78,14 @@ std::string Calculator::gui(std::string& str, bool var_edit) {
 }
 
 std::string Calculator::testinterface(std::string str) {
-  Calculator* calculator = new Calculator();
+  Calculator *calculator = new Calculator();
   std::vector<std::string> input = Calculator::splitString(str);
   std::string result = calculator->calculate(input);
   delete calculator;
-  return result; 
+  return result;
 }
 
-std::string Calculator::write_var(std::string& key, std::string& value) {
+std::string Calculator::write_var(std::string &key, std::string &value) {
   std::ofstream vars;
   vars.open(this->vardir, std::ios_base::app);
   if (vars.is_open()) {
@@ -91,26 +93,25 @@ std::string Calculator::write_var(std::string& key, std::string& value) {
     vars << value << std::endl;
     this->read_vars();
     return "added " + key + " to variables with value " + value + "\n";
-  }
-  else return "Unable to open file\n";
+  } else
+    return "Unable to open file\n";
 }
 
-std::string Calculator::delete_vars(std::string& delkey) {
-  std::map<std::string , std::string>::iterator it;
-  if(this->vars.find(delkey) != this->vars.end()) {
-      this->vars.erase(delkey);
+std::string Calculator::delete_vars(std::string &delkey) {
+  std::map<std::string, std::string>::iterator it;
+  if (this->vars.find(delkey) != this->vars.end()) {
+    this->vars.erase(delkey);
   } else {
     return "The variable " + delkey + " was not found in the list!\n";
   }
-  
-  std::ofstream vars (this->vardir);
+
+  std::ofstream vars(this->vardir);
   if (vars.is_open()) {
-    for(auto e : this->vars) {
-    vars << e.first << std::endl;
-    vars << e.second << std::endl;
+    for (auto e : this->vars) {
+      vars << e.first << std::endl;
+      vars << e.second << std::endl;
     }
-  }
-  else {
+  } else {
     return "Unable to open file";
   }
   return "deleted the variable " + delkey + "\n";
@@ -120,26 +121,25 @@ void Calculator::read_vars() {
   this->vars.clear();
   std::string key;
   std::string value;
-  std::ifstream vars (this->vardir);
+  std::ifstream vars(this->vardir);
   if (vars.is_open()) {
-    while(getline(vars,key)) {
-    getline(vars,value);
-    this->vars.insert(std::pair<std::string,std::string>(key,value));
+    while (getline(vars, key)) {
+      getline(vars, value);
+      this->vars.insert(std::pair<std::string, std::string>(key, value));
     }
     vars.close();
-  }
-  else std::cout << "Unable to open file";
+  } else
+    std::cout << "Unable to open file";
 }
 
 void Calculator::push_vars() {
-  std::map<std::string , std::string>::iterator it;
-  for(auto &e : this->tokens) {
+  std::map<std::string, std::string>::iterator it;
+  for (auto &e : this->tokens) {
     it = this->vars.find(e);
-    if(it != this->vars.end()) {
+    if (it != this->vars.end()) {
       e = it->second;
     }
   }
-
 }
 
 std::vector<std::string> Calculator::splitString(std::string &input) {
@@ -148,24 +148,25 @@ std::vector<std::string> Calculator::splitString(std::string &input) {
   bool digitLock = false;
   bool charLock = false;
   bool dotLock = false;
-  for(auto &e : input){
-    if(std::isdigit(e) ||( e == '.' && !dotLock)){
-      if(charLock) {
+  for (auto &e : input) {
+    if (std::isdigit(e) || (e == '.' && !dotLock)) {
+      if (charLock) {
         charLock = false;
         tokens.push_back(buffer);
         buffer.clear();
       }
-      if(e == '.') dotLock = true;
+      if (e == '.')
+        dotLock = true;
       digitLock = true;
       buffer += e;
       continue;
-    } else if(digitLock){
+    } else if (digitLock) {
       digitLock = false;
       tokens.push_back(buffer);
       buffer.clear();
     }
-    if(Calculator::isOperator(e)){
-      if(charLock) {
+    if (Calculator::isOperator(e)) {
+      if (charLock) {
         charLock = false;
         tokens.push_back(buffer);
         buffer.clear();
@@ -173,70 +174,78 @@ std::vector<std::string> Calculator::splitString(std::string &input) {
       buffer += e;
       tokens.push_back(buffer);
       buffer.clear();
-    } else if(e != ' ') {
+    } else if (e != ' ') {
       buffer += e;
       charLock = true;
-    } else if(charLock) {
+    } else if (charLock) {
       charLock = false;
       tokens.push_back(buffer);
       buffer.clear();
     }
   }
-  if(buffer != ""){
+  if (buffer != "") {
     tokens.push_back(buffer);
   }
   return tokens;
 }
 
-bool Calculator::isOperator(char& op){
-  switch(op){
-    case '%': return true;
-    case '/': return true;
-    case '+': return true;
-    case '-': return true;
-    case '*': return true;
-    case '(': return true;
-    case ')': return true;
-    default: return false;
+bool Calculator::isOperator(char &op) {
+  switch (op) {
+  case '%':
+    return true;
+  case '/':
+    return true;
+  case '+':
+    return true;
+  case '-':
+    return true;
+  case '*':
+    return true;
+  case '(':
+    return true;
+  case ')':
+    return true;
+  default:
+    return false;
   }
 }
 
-std::string Calculator::calculate(std::vector<std::string>& input) {
+std::string Calculator::calculate(std::vector<std::string> &input) {
   this->tokens = input;
-  try{
+  try {
     this->push_vars();
     this->next();
     std::string result = std::to_string(this->handleExpression());
-    result.erase ( result.find_last_not_of('0') + 1, std::string::npos );
-    result.erase ( result.find_last_not_of('.') + 1, std::string::npos );
+    result.erase(result.find_last_not_of('0') + 1, std::string::npos);
+    result.erase(result.find_last_not_of('.') + 1, std::string::npos);
     return result;
-  } catch(ErrorCode code){
-    switch(code){
-      case DIVBYZERO:
-        return "Division and Modulo by 0 is not allowed!";
-      case NOTANOPERATOR:
-        return "Expected an operator at " + this->current;
-      case NOTANUMBER:
-        return "Expected a number at " + this->current;
-      case BRACKEDERROR:
-        return "Open bracket not closed!";
+  } catch (ErrorCode code) {
+    switch (code) {
+    case DIVBYZERO:
+      return "Division and Modulo by 0 is not allowed!";
+    case NOTANOPERATOR:
+      return "Expected an operator at " + this->current;
+    case NOTANUMBER:
+      return "Expected a number at " + this->current;
+    case BRACKEDERROR:
+      return "Open bracket not closed!";
     }
     return "Something went wrong...";
   }
   return "Something went wrong...";
 }
 
-Calculator::Calculator(){}
+Calculator::Calculator() {}
 
-bool Calculator::hasNext(){
-  if(!this->tokens.empty()){
+bool Calculator::hasNext() {
+  if (!this->tokens.empty()) {
     return true;
   }
   return false;
 }
 
-void Calculator::next(){
-  if(this->hasNext()){
+void Calculator::next() {
+  if (this->hasNext()) {
     this->current = this->tokens.front();
     tokens.erase(tokens.begin());
   } else {
@@ -244,75 +253,77 @@ void Calculator::next(){
   }
 }
 
-double Calculator::handleExpression(){
+double Calculator::handleExpression() {
   double result = this->handleTerm();
-  if(!std::isdigit(this->current.front())){
-    while(this->current != "" && this->current != ")"){
-      switch(this->current.front()){
-        case '+':
-          this->next();
-          result += this->handleTerm();
-          break;
-        case '-':
-          this->next();
-          result -= this->handleTerm();
-          break;
-        default:
-          throw(NOTANOPERATOR);
-          break;
+  if (!std::isdigit(this->current.front())) {
+    while (this->current != "" && this->current != ")") {
+      switch (this->current.front()) {
+      case '+':
+        this->next();
+        result += this->handleTerm();
+        break;
+      case '-':
+        this->next();
+        result -= this->handleTerm();
+        break;
+      default:
+        throw(NOTANOPERATOR);
+        break;
       }
     }
   }
   return result;
 }
 
-double Calculator::handleTerm(){
+double Calculator::handleTerm() {
   double result = this->handleFactor();
   double div = 0;
-  switch(this->current.front()){
-    case '*':
-      this->next();
-      result *= this->handleTerm();
-      break;
-    case '/':
-      this->next();
-      div =  this->handleFactor();
-      if(div == 0)throw(DIVBYZERO);
-      result /= div;
-      if(isOperator(this->current.front())){
-        this->tokens.insert(tokens.begin() , this->current);
-        this->current = std::to_string(result);
-        result = this->handleTerm();
-      }
-      break;
-    case '%':
-      this->next();
-      div =  this->handleFactor();
-      if(div == 0)throw(DIVBYZERO);
-      result = (double)((int)result % (int)div);
-      if(isOperator(this->current.front())){
-        this->tokens.insert(tokens.begin() , this->current);
-        this->current = std::to_string(result);
-        result = this->handleTerm();
-      }
-      break;
-    default:
-      break;
+  switch (this->current.front()) {
+  case '*':
+    this->next();
+    result *= this->handleTerm();
+    break;
+  case '/':
+    this->next();
+    div = this->handleFactor();
+    if (div == 0)
+      throw(DIVBYZERO);
+    result /= div;
+    if (isOperator(this->current.front())) {
+      this->tokens.insert(tokens.begin(), this->current);
+      this->current = std::to_string(result);
+      result = this->handleTerm();
+    }
+    break;
+  case '%':
+    this->next();
+    div = this->handleFactor();
+    if (div == 0)
+      throw(DIVBYZERO);
+    result = (double)((int)result % (int)div);
+    if (isOperator(this->current.front())) {
+      this->tokens.insert(tokens.begin(), this->current);
+      this->current = std::to_string(result);
+      result = this->handleTerm();
+    }
+    break;
+  default:
+    break;
   }
   return result;
 }
 
-double Calculator::handleFactor(){
+double Calculator::handleFactor() {
   double result = 0;
-  if(this->current == "("){
+  if (this->current == "(") {
     this->next();
     result = this->handleExpression();
-    if(this->current != ")"){
+    if (this->current != ")") {
       throw(BRACKEDERROR);
     }
     this->next();
   } else {
-    if(!std::isdigit(this->current.front())){
+    if (!std::isdigit(this->current.front())) {
       throw(NOTANUMBER);
     }
     result = std::stod(this->current);
@@ -321,30 +332,24 @@ double Calculator::handleFactor(){
   return result;
 }
 
-std::vector<std::string> Calculator::get_tokens() {
-  return this->tokens;
-}
+std::vector<std::string> Calculator::get_tokens() { return this->tokens; }
 
-std::map<std::string, std::string> Calculator::get_vars() {
-  return this->vars;
-}
+std::map<std::string, std::string> Calculator::get_vars() { return this->vars; }
 
 int testat_interface(std::string expr) {
-  Calculator* calculator = new Calculator();
+  Calculator *calculator = new Calculator();
   std::vector<std::string> input = Calculator::splitString(expr);
   int result = std::stoi(calculator->calculate(input));
   delete calculator;
   return result;
 }
 
-auto calc(int first, int second,char  op) -> int {
+auto calc(int first, int second, char op) -> int {
   std::string expr = std::to_string(first) + op + std::to_string(second);
   return testat_interface(expr);
 }
 
-auto calc(std::istream & stream) -> int {
+auto calc(std::istream &stream) -> int {
   std::string expr(std::istreambuf_iterator<char>(stream), {});
   return testat_interface(expr);
 }
-
-
